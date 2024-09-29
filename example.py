d1387158c506d73153d1e06674a90cc764856982
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-09-29 22:20:38 +0900

    add: example

---
+++ example.py
@@ -0,0 +1,6 @@
+import PayGmailScraper
+
+ana_pay_list = PayGmailScraper.get_payments_ana_pay()
+print(ana_pay_list)
+rakuten_pay_list = PayGmailScraper.get_payments_rakuten_pay()
+print(rakuten_pay_list)
